756400825ffb2bdd4d1914ea1165c099d6850f92
souza99 <jpfsouza99@gmail.com>
2022-11-27 22:40:37 -0300

    Classe com construtor

--- classe.dart
+++ classe.dart
@@ -24,10 +24,12 @@ class Produto {
 }
 
 main() {
-  var produto1 = Produto('Lapis', 1200);
+  var produto1 = Produto('Monitor', 1200);
+  var produto2 = Produto('Celular', 1700);
 
   // produto1.nome = 'Monitor';
   // produto1.preco = 1200;
 
   print("O produto ${produto1.nome} está custando R\$${produto1.preco}");
+  print("O produto ${produto2.nome} está custando R\$${produto2.preco}");
 }
